14f92e728b5f1c0c96919604363168aa07cfbaa4
ffffffklj <1072149188@qq.com>
2022-12-06 22:25:08 +0800

    Update some test case

--- junitTest/Condition_test.java
+++ junitTest/Condition_test.java
@@ -16,15 +16,24 @@ public class Condition_test {
 
         Row r1 = new Row(new String[] { "Luca", "460", "Muse", "SDS", "2020" });
         Row r2 = new Row(new String[] { "Philip", "590", "Muse", "SSE", "2020" });
-        Row r3 = new Row(new String[] {"Luca"});
+        Row r3 = new Row(new String[] {"Luca", "111"});
         table.add(r1);
         table.add(r2);
         Column col1 = new Column("student", table);
+        Column col2 = new Column("id", table);
         Condition cond1 = new Condition(col1, "=", "Luca");
+        Condition cond2 = new Condition(col2, "=", "460");
+
+        // test student = Luca
         List<Condition> condList1 = new ArrayList<Condition>();
         condList1.add(cond1);
         assertTrue(Condition.test(condList1, r3));
         assertTrue(Condition.test(condList1, r1));
         assertNotEquals(true, Condition.test(condList1, r2));
+
+        // test id = 590
+        condList1.add(cond2);
+        assertNotEquals(true, Condition.test(condList1, r3));
+        assertTrue(Condition.test(condList1, r1));
     }
 }
