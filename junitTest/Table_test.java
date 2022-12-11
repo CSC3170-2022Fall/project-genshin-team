e75d00f8bec99a599ece8fbe6aa7e2a8bd206a9d
ffffffklj <1072149188@qq.com>
2022-12-11 16:04:12 +0800

    Update

--- junitTest/Table_test.java
+++ junitTest/Table_test.java
@@ -69,7 +69,7 @@ public class Table_test {
 
     @Test
     public void test_print() {
-        db61b.Table table = db61b.Table.readTable("./testing/enrolled");
+        db61b.Table table = db61b.Table.readTable("./pre/job_history");
         table.print();
         // assert???
     }
